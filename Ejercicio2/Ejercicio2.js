a8517792f334b986d736c3d4cfd7e0d8c0f741c6
juancardaw <160797014+juancardaw@users.noreply.github.com>
2024-11-23 16:47:50 +0100

    Add files via upload

---
+++ Ejercicio2/Ejercicio2.js
@@ -0,0 +1,7 @@
+// 2.1 Dado el siguiente array, crea una copia usando spread operators.
+
+const pointsList = [32, 54, 21, 64, 75, 43];
+
+const copiaArray = [...pointsList];
+console.log(copiaArray);
+
